c25fd6a72e8dabccdc1d801e70d678d749c644fc
Drew <aamunat2@jh.edu>
2022-06-08 16:51:08 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import Title from "./components/Title";
 import Search from "./components/Search";
 import Characters from "./components/Characters";
