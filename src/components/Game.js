3a7c5f162f8e692ddc87c8fa625d33457e349c4a
Drew <aamunat2@jh.edu>
2022-06-13 13:03:05 -0400

    game container turned into vertical grid

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,5 @@
 import { Container } from "@mui/system";
+import { Grid } from "@mui/material";
 import PropTypes from "prop-types";
 import Character from "../Character";
 
@@ -28,32 +29,55 @@ function Game(props) {
   }
 
   return (
-    <div className='characters-container'>
-      <Container className="is-max-widescreen" id="characters" disableGutters={true}>
-        <section className="section is-small">
-          {
-            characters.map((character, index) => {
-              colorMap = colors.find(item => item.key === character.name);
-              return (
-                <Character
-                  index={index}
-                  character={character}
-                  nameColor={colorMap.colors.name}
-                  genderColor={colorMap.colors.gender}
-                  showColor={colorMap.colors.show}
-                  genreColor={colorMap.colors.genre}
-                  platformColor={colorMap.colors.platform}
-                  roleColor={colorMap.colors.role}
-                  yearColor={colorMap.colors.year}
-                  arrow={arrow(character.characteristics.year)}
-                  icon={icon(character.characteristics.year)}
-                />
-              )
-            })
-          }
-        </section>
-      </Container>
-    </div>
+
+    <Grid container>
+      {
+        characters.map((character, index) => {
+          colorMap = colors.find(item => item.key === character.name);
+          return (
+            <Grid container item>
+              <Character
+                index={index}
+                character={character}
+                nameColor={colorMap.colors.name}
+                genderColor={colorMap.colors.gender}
+                showColor={colorMap.colors.show}
+                genreColor={colorMap.colors.genre}
+                platformColor={colorMap.colors.platform}
+                roleColor={colorMap.colors.role}
+                yearColor={colorMap.colors.year}
+                arrow={arrow(character.characteristics.year)}
+                icon={icon(character.characteristics.year)}
+              />
+            </Grid>
+          )
+        })
+      }
+    </Grid>
+
+
+    /*<Container className="is-max-widescreen" id="characters" disableGutters={true}>
+        {
+          characters.map((character, index) => {
+            colorMap = colors.find(item => item.key === character.name);
+            return (
+              <Character
+                index={index}
+                character={character}
+                nameColor={colorMap.colors.name}
+                genderColor={colorMap.colors.gender}
+                showColor={colorMap.colors.show}
+                genreColor={colorMap.colors.genre}
+                platformColor={colorMap.colors.platform}
+                roleColor={colorMap.colors.role}
+                yearColor={colorMap.colors.year}
+                arrow={arrow(character.characteristics.year)}
+                icon={icon(character.characteristics.year)}
+              />
+            )
+          })
+        }
+    </Container>*/
   );
 }
 
