ab03d34dbaa833bebf7b78171922ef8ccd713d89
Drew <aamunat2@jh.edu>
2022-07-07 18:31:55 -0400

    added universe

--- src/components/Search.js
+++ src/components/Search.js
@@ -148,7 +148,7 @@ class Search extends Component {
     if(guess.characteristics.platform === this.props.solution.characteristics.platform) {
       colorObj.platform = "#6CA663";
       colorObj.platformColor = 'green';
-    } else if(guess.characteristics.owner === this.props.solution.characteristics.owner) {
+    } else if(guess.characteristics.universe === this.props.solution.characteristics.universe) {
       colorObj.platform = "#E0CA3C";
       colorObj.platformColor = 'yellow';
     }
