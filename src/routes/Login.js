aeeb4aac31ae17e458d119ec7982beb41bba21e3
Drew <aamunat2@jh.edu>
2022-07-22 17:59:38 -0400

    login form has error handling

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import axios from 'axios'
 import { useSignIn } from 'react-auth-kit'
 import { useNavigate, Navigate } from 'react-router-dom'
@@ -7,8 +7,6 @@ import {useIsAuthenticated} from 'react-auth-kit';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -29,36 +27,72 @@ function Copyright(props) {
   );
 }
 
+function FormError(props) {
+
+  const {showIncorrectError, showUsernameError, showPasswordError} = props;
+
+  const showMessage = () => {
+    if(showUsernameError || showPasswordError) {
+      if(showUsernameError && showPasswordError) {
+        return "username and password can't be empty";
+      } else if (showUsernameError) {
+        return "username can't be empty";
+      } else {
+        return "password can't be empty"
+      }
+    } else if (showIncorrectError) {
+      return 'incorrect username or password';
+    } else {
+      return '';
+    }
+  }
+
+  return (
+    <>
+      <Typography component="h7" variant="h" color='error'>
+        {showMessage()}
+      </Typography>
+    </>
+  );
+}
+
 const theme = createTheme();
 
 export default function Login () {
   const navigate = useNavigate()
   const signIn = useSignIn()
   const isAuthenticated = useIsAuthenticated()
+  const [showIncorrectError, setShowIncorrectError] = useState(false)
+  const [showUsernameError, setShowUsernameError] = useState(false)
+  const [showPasswordError, setShowPasswordError] = useState(false)
 
   const onSubmit = (e) => {
     const data = new FormData(e.currentTarget);
+    if(data.get('username') !== '' && data.get('password') !== ''){
+    setShowUsernameError(false);
+    setShowPasswordError(false);
     const formData = {username: data.get('username'), password: data.get('password'),}
     console.log(formData)
       e.preventDefault()
       axios.post('http://localhost:5050/authenticate', formData)
-          .then((res)=>{
-            console.log("successful post")
-              if(res.status === 200){
-                  console.log("status = 200", res.data)
-                  if(signIn({token: res.data.token,
-                              expiresIn: 120,
-                              tokenType: "Bearer",
-                              authState: {name: res.data.name, uid: res.data.uid},
-                              /*refreshToken: res.data.refreshToken,                    // Only if you are using refreshToken feature
-                              refreshTokenExpireIn: res.data.refreshTokenExpireIn*/})){ // Only if you are using refreshToken feature
-                    console.log('signed in', isAuthenticated())  
-                    navigate("/admin");
-                  }else {
-                    alert("Error Occoured. Try Again");
-                  }
-              }
-          })
+      .then((res)=>{
+          if(res.status === 200){
+              if(signIn({token: res.data.token,
+                          expiresIn: 120,
+                          tokenType: "Bearer",
+                          authState: {name: res.data.name, uid: res.data.uid},
+                          /*refreshToken: res.data.refreshToken,                    // Only if you are using refreshToken feature
+                          refreshTokenExpireIn: res.data.refreshTokenExpireIn*/})){ // Only if you are using refreshToken feature 
+                navigate("/admin");
+                setShowIncorrectError(false);
+              }else {
+                alert("Error Occoured. Try Again");
+                setShowIncorrectError(true);
+              }}
+      }).catch(() => {setShowIncorrectError(true)})} else {
+        setShowUsernameError(data.get('username') === '');
+        setShowPasswordError(data.get('password') === '');
+      }
   }
 
   if(isAuthenticated()){
@@ -66,16 +100,15 @@ export default function Login () {
   } else {
     return (
       <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <Container maxWidth="xs">
         <Box
           sx={{
-            marginTop: 8,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, mt: 8 , bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
@@ -102,9 +135,10 @@ export default function Login () {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
+            <FormError 
+              showIncorrectError={showIncorrectError}
+              showUsernameError={showUsernameError}
+              showPasswordError={showPasswordError}
             />
             <Button
               type="submit"
@@ -116,17 +150,9 @@ export default function Login () {
             </Button>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
+        <Copyright sx={{ mt: 8 }} />
       </Container>
     </ThemeProvider>
     );
-    /*return (
-      <form onSubmit={onSubmit}>
-          <input type={"email"} onChange={(e)=>setFormData({...formData, username: e.target.value})}/>
-          <input type={"password"} onChange={(e)=>setFormData({...formData, password: e.target.value})}/>
-
-          <button>Submit</button>
-      </form>
-    )*/
   }   
 }
